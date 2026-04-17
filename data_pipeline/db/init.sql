34706101244434a44d1771aebf0473585df44398
h4zan <h4zan@hotmail.com>
2026-04-17 09:47:35 +0200

    changing init

--- data_pipeline/db/init.sql
+++ data_pipeline/db/init.sql
@@ -2,32 +2,27 @@
 CREATE EXTENSION IF NOT EXISTS timescaledb;
 
 -- ----------------------------
--- Metadata Table
--- ----------------------------
-CREATE TABLE IF NOT EXISTS component (
-    component_id BIGSERIAL PRIMARY KEY,
-    serial_number TEXT NOT NULL UNIQUE,
-    model TEXT NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT now()
-);
-
--- ----------------------------
 -- Time-Series Table
 -- ----------------------------
-CREATE TABLE IF NOT EXISTS event_data (
+CREATE TABLE IF NOT EXIST robot_data(
     ts TIMESTAMPTZ NOT NULL,
-    component_id BIGINT NOT NULL,
-    temperature DOUBLE PRECISION,
-    humidity DOUBLE PRECISION
---    CONSTRAINT fk_component
---        FOREIGN KEY(component_id)
---        REFERENCES component(component_id)
---        ON DELETE CASCADE
+    device_id INT NOT NULL,
+
+    event_state INT, 
+    state_message TEXT,
+
+    event_value DOUBLE PRECISION,
+
+    error_code INT,
+
+    error_message TEXT
+
 );
 
 -- Convert to hypertable
-SELECT create_hypertable('event_data', 'ts', if_not_exists => TRUE);
+SELECT create_hypertable('robot_data', 'ts', if_not_exists => TRUE);
 
 -- Helpful index
-CREATE INDEX IF NOT EXISTS idx_event_component_time
-ON event_data (component_id, ts DESC);
+CREATE INDEX IF NOT EXISTS idx_robot_device_time
+ON robot_data (device_id, ts DESC);
+
